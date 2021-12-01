e16e65c169576a6d0c918a04c260b128502bc01b
Aleksandra Bošković <sandraboskovic@hotmail.com>
2021-12-01 05:23:20 +0100

    update/remove mongoose options

--- configs/db.config.js
+++ configs/db.config.js
@@ -1,10 +1,6 @@
 const mongoose = require("mongoose");
 
 mongoose
-  .connect("mongodb://localhost/movies-db-dev", {
-    useCreateIndex: true,
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  })
+  .connect("mongodb://localhost/movies-db-dev")
   .then((x) => console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`))
   .catch((err) => console.error("Error connecting to mongo", err));
